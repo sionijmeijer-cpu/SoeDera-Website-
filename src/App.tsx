b458f6effee4ffc309ad339929e9662543f34da0
Sylvia Awoudu <sionijmeijer@gmail.com>
2026-04-21 12:40:30 +0100

    Refactor blog routes and update BlogPost to BlogDetail

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Services from './pages/Services';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
+import BlogDetail from './pages/BlogDetail';
 import FAQ from './pages/FAQ';
 import Brochure from './pages/Brochure';
 import Contact from './pages/Contact';
@@ -20,21 +20,6 @@ import BIMManagement from './pages/services/BIMManagement';
 import ProductDevelopment from './pages/services/ProductDevelopment';
 import AssetManagement from './pages/services/AssetManagement';
 import ProjectManagement from './pages/services/ProjectManagement';
-import DocumentManagementArticle from './pages/articles/DocumentManagement';
-import ReferenceDesignationArticle from './pages/articles/ReferenceDesignation';
-import ProjectManagementArticle from './pages/articles/ProjectManagement';
-import CloudInformationManagementArticle from './pages/articles/CloudInformationManagement';
-import PredictiveMaintenanceArticle from './pages/articles/PredictiveMaintenance';
-
-import DigitalTransformationArticle from './pages/articles/DigitalTransformation';
-import SAMIntroductionArticle from './pages/articles/SAMIntroduction';
-import BlogArticleBIM from './components/BlogArticleBIM';
-import BlogArticleDocMgmt from './components/BlogArticleDocMgmt';
-import BlogArticleAgile from './components/BlogArticleAgile';
-import BlogArticleDigitalTransform from './components/BlogArticleDigitalTransform';
-import BlogArticlePredictive from './components/BlogArticlePredictive';
-import BlogArticleRDS from './components/BlogArticleRDS';
-import BlogArticleCloudInfo from './components/BlogArticleCloudInfo';
 
 function App() {
   return (
@@ -45,7 +30,17 @@ function App() {
         <Header />
         <main className="flex-1">
           <Routes>
+            {/* Core pages */}
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/brochure" element={<Brochure />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/terms" element={<TermsOfService />} />
+
+            {/* Services */}
             <Route path="/services" element={<Services />} />
             <Route path="/services/document-management" element={<DocumentManagement />} />
             <Route path="/services/rds" element={<RDS />} />
@@ -53,30 +48,10 @@ function App() {
             <Route path="/services/product-development" element={<ProductDevelopment />} />
             <Route path="/services/asset-management" element={<AssetManagement />} />
             <Route path="/services/project-management" element={<ProjectManagement />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:id" element={<BlogPost />} />
-            <Route path="/blog/sam-introduction" element={<SAMIntroductionArticle />} />
-            <Route path="/blog/document-management" element={<DocumentManagementArticle />} />
-            <Route path="/blog/reference-designation" element={<ReferenceDesignationArticle />} />
-            <Route path="/blog/project-management" element={<ProjectManagementArticle />} />
-            <Route path="/blog/cloud-information-management" element={<CloudInformationManagementArticle />} />
-            <Route path="/blog/predictive-maintenance" element={<PredictiveMaintenanceArticle />} />
 
-            <Route path="/blog/digital-transformation" element={<DigitalTransformationArticle />} />
-            <Route path="/blog/bim-trends" element={<BlogArticleBIM />} />
-            <Route path="/blog/document-management" element={<BlogArticleDocMgmt />} />
-            <Route path="/blog/agile-waterfall" element={<BlogArticleAgile />} />
-            <Route path="/blog/digital-transformation" element={<BlogArticleDigitalTransform />} />
-            <Route path="/blog/predictive-maintenance" element={<BlogArticlePredictive />} />
-            <Route path="/blog/rds-standards" element={<BlogArticleRDS />} />
-            <Route path="/blog/cloud-information" element={<BlogArticleCloudInfo />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/brochure" element={<Brochure />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsOfService />} />
+            {/* Blog — ALL articles go through BlogDetail via :id slug */}
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogDetail />} />
           </Routes>
         </main>
         <Footer />
